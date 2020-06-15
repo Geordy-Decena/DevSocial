f8281c5f595e277358f53637dfc8f3fd60ca398c
Geordy Decena <geordydecena@Geordys-MacBook-Pro.local>
2020-06-15 03:01:23 -0400

    final lines

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -3,11 +3,12 @@ import { setAlert } from './alert'
 import {
     GET_POSTS,
     POST_ERROR,
-    UPDATE_PROFILE,
     UPDATE_LIKES,
     DELETE_POST,
     ADD_POST,
-    GET_POST
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from './types'
 
 export const getPosts = () => async dispatch => {
@@ -106,6 +107,48 @@ export const getPost = id => async dispatch => {
             type: GET_POST,
             payload: res.data
         })
+    }
+    catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+export const addComment = (postId, formData) => async dispatch => {
+    const config = {
+        header: {
+            'Content-Type': 'application/json'
+        }
+    }
+    try {
+        const res = await axios.post(`/api/posts/comment/${postId}`, formData, config)
+        dispatch({
+            type: ADD_COMMENT,
+            payload: res.data
+        })
+
+        dispatch(setAlert('Comment Added', 'success'))
+    }
+    catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+export const deleteComment = (postId, commentId) => async dispatch => {
+
+    try {
+        const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`)
+        dispatch({
+            type: REMOVE_COMMENT,
+            payload: commentId
+        })
+
+        dispatch(setAlert('Comment Removed', 'success'))
     }
     catch (err) {
         dispatch({
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,13 +4,23 @@ import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import PostItem from '../posts/PostItem'
 import { getPost } from '../../actions/post'
+import { Link } from 'react-router-dom'
+import CommentForm from './CommentForm'
+import CommentItem from './CommentItem'
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
     useEffect(() => {
         getPost(match.params.id)
     }, [getPost])
     return loading || post === null ? <Spinner /> : <Fragment>
+        <Link to='/posts' className='btn'>Back to Post</Link>
         <PostItem post={post} showActions={false} />
+        <CommentForm postId={post._id} />
+        <div className="comments">
+            {post.comments.map(comment => (
+                <CommentItem key={comment._id} comment={comment} postId={post._id} />
+            ))}
+        </div>
     </Fragment>
 }
 
